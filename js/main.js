1ab63e9dc332186ef469a16e21f507fc55a2fdfb
Ana Fernandez <anafdz92@gmail.com>
2020-07-10 10:54:44 +0200

    add to favourites array

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 "use strict";
 
+//Creamos dúas variables valeiras.
+
 let series = [];
 let favourites = [];
 
-// Obter os datos da Api
+// 1.OBTER DATOS DA API
 
-//Creamos a referencia o botón
+//1.1Creamos a referencia o botón
 
 const buttonSearch = document.querySelector(".js-search-button");
 
-//Función para coller os datos da Api
+//1.2Función para coller os datos da Api
 
 const getDataFromApi = () => {
-  //Creamos unha referencia o valor do imput
-
+  //1.3 Creamos unha referencia o valor do imput
   const serieName = document.querySelector(".js-search__text");
 
   fetch(`http://api.tvmaze.com/search/shows?q=:${serieName.value}`)
@@ -25,11 +26,11 @@ const getDataFromApi = () => {
     });
 };
 
-//Engadir eventListener o botón
+//1.4 Engadir eventListener o botón
 
 buttonSearch.addEventListener("click", getDataFromApi);
 
-//Creamos unha función para pintar
+//2. CREAMOS UNHA FUNCIÓN PARA PINTAR
 
 const paintSeries = () => {
   let codeHTML = "";
@@ -40,7 +41,7 @@ const paintSeries = () => {
     } else {
       imgSerie = series[index].show.image.medium;
     }
-    codeHTML += `<li>`;
+    codeHTML += `<li id = "${series[index].show.id}" class = "serieStyle">`;
     codeHTML += `<article>`;
     codeHTML += `<img src= "${imgSerie}" class ="card__img" alt="Foto de ${series[index].show.name}"/> `;
     codeHTML += `<p> ${series[index].show.name}</p>`;
@@ -49,4 +50,26 @@ const paintSeries = () => {
   }
   const cartSeries = document.querySelector(".js__elements");
   cartSeries.innerHTML = codeHTML;
+  listenSeries();
 };
+
+//3. SELECCIONAR FAVORITA.
+
+function listenSeries() {
+  let selectSeries = document.querySelectorAll(".serieStyle");
+  console.log(selectSeries);
+
+  for (const serie of selectSeries) {
+    serie.addEventListener("click", changeFavourite);
+  }
+}
+function changeFavourite(ev) {
+  let clickedSerie = parseInt(ev.currentTarget.id);
+  for (let i = 0; i < series.length; i++) {
+    console.log("hola", clickedSerie, series[i].show.id);
+    if (clickedSerie === series[i].show.id) {
+      favourites.push(series[i].show);
+    }
+  }
+  console.log(favourites);
+}
