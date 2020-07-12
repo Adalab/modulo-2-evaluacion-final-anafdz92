84fdf06e8c27de24fbe48fa1aa6741bbfe7bee99
Ana Fernandez <anafdz92@gmail.com>
2020-07-12 13:54:10 +0200

    solve change color favourites and remove from fav

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 "use strict";
 
-//Creamos dúas variables valeiras.
+//Creamos dous arrays valeiros.
 
 let series = [];
 let favourites = [];
 
 // 1.OBTER DATOS DA API
 
-//1.1Creamos a referencia o botón
+//1.1Creamos a referencia o botón e engadimos o eventListener.
+//Mellor nunha función para telo todo en paquetiños
+
+// const listenSearchButton = () => {
+
+//   const buttonSearch = document.querySelector(".js-search-button");
+//   buttonSearch.addEventListener("click", getDataFromApi);
+
+// }
 
 const buttonSearch = document.querySelector(".js-search-button");
 
@@ -36,7 +44,7 @@ const paintSeries = () => {
   let firstListCodeHTML = "";
   let secondListCodeHTML = "";
   let imgSerie;
-  let favClass;
+  let favClass = `js__serieStyle`;
 
   //Se non hai series favoritas borramos "Mis series favoritas" e o reset
   showFavContainer();
@@ -51,17 +59,18 @@ const paintSeries = () => {
       imgSerie = series[index].show.image.medium;
     }
     //CONDICIONAL PARA QUE SE POÑA VERDE SE ESTÁ NO ARRAY DE FAVORITOS.
-    //Necesitaría engadir favClass no HMTL indentado coma clase ${favClass} pero devolve "undefined".
+    //Necesitaría engadir favClass no HMTL indentado coma clase ${favClass}.
 
-    // for (let index = 0; index < favourites.length; index += 1) {
-    //   if (favourites[index].show.id === series[index].show.id) {
-    //     favClass = `js__serieStyle`; //undefined
-    //   } else {
-    //     favClass = `js__serieStyleFavourite`;
-    //   }
-    // }
+    let serie = series[index];
+    for (let i = 0; i < favourites.length; i += 1) {
+      if (serie.show.id === favourites[i].show.id) {
+        favClass = `js__serieStyleFavourite`;
+      } else {
+        favClass = `js__serieStyle`;
+      }
+    }
 
-    firstListCodeHTML += `<li id = "${series[index].show.id}" class = "js__serieStyle">`;
+    firstListCodeHTML += `<li id = "${series[index].show.id}" class = ${favClass}>`;
     firstListCodeHTML += `<article>`;
     firstListCodeHTML += `<img src= "${imgSerie}" class ="card__img" alt="Foto de ${series[index].show.name}"/> `;
     firstListCodeHTML += `<p> ${series[index].show.name}</p>`;
@@ -102,7 +111,6 @@ const paintSeries = () => {
 
 function showFavContainer() {
   if (favourites.length > 0) {
-    debugger;
     const titleFav = document.querySelector(`.title`);
     titleFav.classList.remove(`hidden`);
     const ResetBtn = document.querySelector(`.resetBtn`);
@@ -113,7 +121,9 @@ function showFavContainer() {
 //3. SELECCIONAR FAVORITA.
 
 function listenSeries() {
-  let selectSeries = document.querySelectorAll(".js__serieStyle");
+  let selectSeries = document.querySelectorAll(
+    ".js__serieStyle, .js__serieStyleFavourite"
+  );
   console.log(selectSeries);
 
   for (const serie of selectSeries) {
@@ -127,8 +137,20 @@ function changeFavourite(ev) {
   for (let i = 0; i < series.length; i++) {
     console.log("hola", clickedSerie, series[i].show.id);
     if (clickedSerie === series[i].show.id) {
-      favourites.push(series[i]);
-      //series[i].classList.add(".js__serieStyleFavourite"); //cambiar a verde?
+      //Buscamos en favourites o obxecto cuxo id sexa igual co de series
+      let serieFav = favourites.find(
+        (favourite) => favourite.show.id == series[i].show.id
+      );
+      // Se o atopamos (se non é undefined) está dentro de favoritos
+      if (serieFav !== undefined) {
+        //Buscamos o index desa serie en favoritos e sacámolo do array
+        let indexInFavourites = favourites.findIndex((fav) => serieFav === fav);
+        favourites.splice([indexInFavourites], 1);
+      }
+      //Se non o atopamos non está en favoritos
+      else {
+        favourites.push(series[i]);
+      }
     }
     updateLocalStorage();
   }
@@ -136,44 +158,11 @@ function changeFavourite(ev) {
   paintSeries();
 }
 
-//VERSIÓN CHANGE FAVOURITE PRA ENGADIR/QUITAR DE FAVORITOS
-//Si favoritos no contiene la serie clickada: métela si la contiene sacala
-// function changeFavourite(ev) {
-//   let clickedSerie = parseInt(ev.currentTarget.id);
-//   for (let i = 0; i < favourites.length; i++) {
-//     if (!favourites[i].show.id === clickedSerie) {
-//       let clickedSerie = parseInt(ev.currentTarget.id);
-//       for (let i = 0; i < series.length; i++) {
-//         console.log("hola", clickedSerie, series[i].show.id);
-//         if (clickedSerie === series[i].show.id) {
-//           //series[i].classList.add(".js__serieStyleFavourite"); y pintala de verde
-//           favourites.push(series[i]);
-//         }
-//       }
-//     } else {
-//       let clickedSerie = parseInt(ev.currentTarget.id);
-//       for (let i = 0; i < series.length; i++) {
-//         console.log("hola", clickedSerie, series[i].show.id);
-//         if (clickedSerie === series[i].show.id) {
-//           //series[i].classList.add("js__serieStyle"); y pintala de lila
-//           favourites.splice([i], 1);
-//         }
-//       }
-//     }
-//     updateLocalStorage();
-//   }
-//   paintSeries();
-// }
-
 //4.LOCAL STORAGE
 
 const updateLocalStorage = () => {
   localStorage.setItem("favourites", JSON.stringify(favourites));
 };
-
-// const removeFromLocalStorage = () => {
-//   localStorage.removeItem("favourites", JSON.stringify(favourites));
-// };
 
 const getFromLocalStorage = () => {
   const data = JSON.parse(localStorage.getItem("favourites"));
@@ -221,7 +210,7 @@ function resetAll() {
   favourites.length = 0;
   window.localStorage.clear();
   paintSeries();
-  //reset y el título desaparecen?
+  //reset e o título desaparecen?
 }
 
 getFromLocalStorage();
